5c83c69dfdc07e41814d49a4735eb9e6957f9307
Sebastian Muszynski <basti@linkt.de>
2025-11-30 15:04:38 +0100

    Fix

--- components/treadmill_f15/treadmill_f15.cpp
+++ components/treadmill_f15/treadmill_f15.cpp
@@ -56,7 +56,8 @@ void TreadmillF15::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t
       auto *char_notify =
           this->parent_->get_characteristic(TREADMILL_BMS_SERVICE_UUID, TREADMILL_BMS_NOTIFY_CHARACTERISTIC_UUID);
       if (char_notify == nullptr) {
-        ESP_LOGE(TAG, "[%s] No notify service found at device, not an Treadmill BMS..?", ADDR_STR(this->parent_->address_str()));
+        ESP_LOGE(TAG, "[%s] No notify service found at device, not an Treadmill BMS..?",
+                 ADDR_STR(this->parent_->address_str()));
         break;
       }
 
@@ -89,7 +90,8 @@ void TreadmillF15::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t
       auto *char_command =
           this->parent_->get_characteristic(TREADMILL_BMS_SERVICE_UUID, TREADMILL_BMS_CONTROL_CHARACTERISTIC_UUID);
       if (char_command == nullptr) {
-        ESP_LOGE(TAG, "[%s] No control service found at device, not an Treadmill BMS..?", ADDR_STR(this->parent_->address_str()));
+        ESP_LOGE(TAG, "[%s] No control service found at device, not an Treadmill BMS..?",
+                 ADDR_STR(this->parent_->address_str()));
         break;
       }
       this->char_command_handle_ = char_command->handle;
